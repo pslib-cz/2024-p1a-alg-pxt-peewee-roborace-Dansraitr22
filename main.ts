5d8d1bddc24c5d1f741f224a6710c54cc3e68b90
Deron22 <Daniel.sraitr.024@pslib.cz>
2025-06-13 13:08:16 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,26 +3,35 @@ type IRC = {
     c: DigitalPin,
     r: DigitalPin
 }
-type PinData = {
-    l: boolean,
-    c: boolean,
-    r: boolean
-}
 const IR: IRC = {
     l: DigitalPin.P14,
     c: DigitalPin.P15,
     r: DigitalPin.P13
 }
+let speed: number = 125
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
-
-let data: PinData = {l: false, c: false, r: false};
+function online(left: number, right: number, center: number) {
+    if (left || right || center === 1) {
+        return true
+    } else {
+        return false
+    }
+}
 basic.forever(function () {
-    data.l = pins.digitalReadPin(IR.l) === 1;
-    data.c = pins.digitalReadPin(IR.c) === 1;
-    data.r = pins.digitalReadPin(IR.r) === 1;
-    console.log(data);
+    if (online(IR.l, IR.r, IR.c)) {
+        if (IR.c === 1) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+        } else if (IR.l === 1) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed + 40)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed - 40)
+        } else if (IR.r === 1)
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed - 40)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed + 40)
+    } else if (!online(IR.l, IR.r, IR.c)) {
+        PCAmotor.MotorStopAll
+    }
 
-    basic.pause(20)
 })
