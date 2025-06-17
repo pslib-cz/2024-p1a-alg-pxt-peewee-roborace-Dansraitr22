3430792b1cf33a38faa71bd30e32b99fce3833fd
Deron22 <Daniel.sraitr.024@pslib.cz>
2025-06-17 20:12:21 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,21 +8,21 @@ const IR: IRC = {
     l: DigitalPin.P13,
     r: DigitalPin.P15
 }
-let speed: number = 140
+let speed: number = 100
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
 function online(left: number, right: number, center: number) {
-    if (left || right  || center === 1) {
+    if (left || right || center === 1) {
         return true
-    } else if (left && right && center===0){
+    } else if (left && right && center === 0) {
         return false
-    }return false
+    } return false
 }
 
 let center: number = 0
-let left:number=0
-let right:number=0
+let left: number = 0
+let right: number = 0
 
 basic.forever(function () {
     center = pins.digitalReadPin(IR.c)
@@ -30,34 +30,36 @@ basic.forever(function () {
     right = pins.digitalReadPin(IR.r)
     if (online(left, right, center)) {
         if (center === 1) {
-            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed  )
-            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
-        } else if (left === 1) {
-            PCAmotor.MotorRun(PCAmotor.Motors.M4, 0 )
             PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-        } else if (right === 1)
-            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed  )
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
-    } else if (!online(left, right, center)) {
-        PCAmotor.MotorStop(PCAmotor.Motors.M1, )
-        PCAmotor.MotorStop(PCAmotor.Motors.M4,)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+        } if (left === 1) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, 0)
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+        } if (right === 1) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
+        }
 
-    } 
+    } else if (!online(left, right, center)) {
+        PCAmotor.MotorStop(PCAmotor.Motors.M1)
+        PCAmotor.MotorStop(PCAmotor.Motors.M4)
+
+    }
 
 })
-radio.onReceivedString (function(name:string){
-    
-    if (name==="leva") {
+radio.onReceivedString(function (name: string) {
+
+    if (name === "leva") {
         PCAmotor.MotorRun(PCAmotor.Motors.M4, speed - 40)
         PCAmotor.MotorRun(PCAmotor.Motors.M1, speed + 20)
     }
-    if (name ==="prava") {
+    if (name === "prava") {
         PCAmotor.MotorRun(PCAmotor.Motors.M4, speed + 20)
         PCAmotor.MotorRun(PCAmotor.Motors.M1, speed - 40)
     }
-    if (name ==="rovne") {
+    if (name === "rovne") {
         PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
         PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
     }
-} 
+}
 )
