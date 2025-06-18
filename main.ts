6760ce044966bd93086723aa53547bd966f5f1b4
Deron22 <Daniel.sraitr.024@pslib.cz>
2025-06-18 14:07:17 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ type IRC = {
     r: DigitalPin
 }
 const IR: IRC = {
-    c: DigitalPin.P14,
-    l: DigitalPin.P13,
-    r: DigitalPin.P15
+    c: DigitalPin.P15,
+    l: DigitalPin.P14,
+    r: DigitalPin.P13
 }
 let speed: number = 120
 pins.setPull(IR.l, PinPullMode.PullNone);
@@ -24,28 +24,29 @@ function online(left: number, right: number, center: number) {
 let center: number = 0
 let left: number = 0
 let right: number = 0
-let cr:boolean=false
+let cr: boolean = false
 basic.forever(function () {
     center = pins.digitalReadPin(IR.c)
     left = pins.digitalReadPin(IR.l)
     right = pins.digitalReadPin(IR.r)
-    if (online(left, right, center)&&cr===false) {
+    if (online(left, right, center) && cr === false) {
         if (center === 1) {
-            PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, -speed)
             PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
         } if (left === 1) {
             PCAmotor.MotorRun(PCAmotor.Motors.M4, 0)
             PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+
         } if (right === 1) {
-            PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, -speed)
             PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
         }
 
     } else if (!online(left, right, center)) {
         PCAmotor.MotorStop(PCAmotor.Motors.M1)
         PCAmotor.MotorStop(PCAmotor.Motors.M4)
-cr=true
-basic.showLeds(`
+        cr = true
+        basic.showLeds(`
 . . . . .
 . . . . .
 . . # . .
@@ -66,20 +67,21 @@ basic.showLeds(`
 radio.onReceivedString(function (name: string) {
 
     if (name === "leva") {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 0 )
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed )
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 0)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+
         cr = false
         basic.pause(1000)
     }
     if (name === "prava") {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed )
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 0 )
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -speed)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
         cr = false
         basic.pause(1000)
     }
     if (name === "rovne") {
         PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -speed)
         cr = false
         basic.pause(1000)
     }
